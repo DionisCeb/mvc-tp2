b8b8e3d6f0d4a092de8eaba5616ad2175d88c5eb
DionisCeb <162382338+DionisCeb@users.noreply.github.com>
2024-08-10 14:48:33 -0400

    Delete mvc directory

--- mvc/assets/js/scrolling.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('a[href="#reserve-sec"]').addEventListener('click', function(event) {
-    event.preventDefault();
-    const targetSection = document.getElementById('reserve-sec');
-    targetSection.scrollIntoView({ behavior: 'smooth' });
-});
--- mvc/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-use App\Controllers\HomeController;
-use App\Routes\Route;
-
-Route::get('/', 'HomeController@index');
-Route::dispatch();
-?>
